9fd848c00b844090a9c27c20073b0eea163d4aa1
Bartek Kieczka <bartek79701@gmail.com>
2024-01-09 22:40:24 +0100

    .

---
+++ Grocery-list/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grocery_list
+{
+    internal class Product
+    {
+        public  Product(int id, string name, double price)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+        }
+        protected int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+    }
+}
--- Grocery-list/Program.cs
+++ Grocery-list/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Grocery_list;
+
+
+
+ void Menu()
+{
+    Console.WriteLine("Welcome in Grocery lis");
+    var flag = true;
+    List<Product> products = new List<Product>();
+    while (flag)
+    {
+        Console.WriteLine("Choose option");
+        Console.WriteLine("1:Open new grocery list");
+        Console.WriteLine("2:Check products list");
+        Console.WriteLine("3:Exit");
+        int input = int.Parse(Console.ReadLine());
+        switch(input)
+        {
+            case 1:
+                {
+                    products.RemoveRange(0, products.Count);
+                    break;
+                }
+
+                case 2: 
+                {
+                    Console.WriteLine("E");
+                    break;
+
+                }
+
+            case 3:
+
+                
+                break;
+
+
+            default: throw new Exception("Bad option");
+        }
+    }
+}
